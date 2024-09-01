6da0f813042f7ce54d071a4baff58c95cab692b8
martin <ash999@abv.bg>
2024-09-01 21:38:28 +0300

    Feat: Dockerize and refactor

--- repository/pet_repository.py
+++ repository/pet_repository.py
@@ -9,21 +9,25 @@ class PetRepository:
         self.session = Session(engine)
 
     def get_pet_by_id(self, pet_id):
+        self.refresh_session()
         pet = self.session.query(PetModel).filter(PetModel.pet_id == pet_id).first()
         if not pet:
             return None
         return pet
 
     def get_all_pets(self):
+        self.refresh_session()
         result = self.session.query(PetModel).all()
         return result
 
     def create_pet(self, pet_data: PetModel):
+        self.refresh_session()
         self.session.add(pet_data)
         self.session.commit()
         return pet_data
 
     def delete_pet(self, pet_id):
+        self.refresh_session()
         selected_pet = self.get_pet_by_id(pet_id)
         if selected_pet:
             self.session.delete(selected_pet)
@@ -32,18 +36,24 @@ class PetRepository:
         return "No such ID"
 
     def get_all_pets_for_user(self, user_id):
+        self.refresh_session()
         found_pets = self.session.query(PetModel).filter(PetModel.owner_id == user_id).all()
         if not found_pets:
             return []
         return found_pets
 
     def change_owner_for_pet(self, pet_id, new_owner_id):
+        self.refresh_session()
         selected_pet = self.get_pet_by_id(pet_id)
         if selected_pet:
             selected_pet.owner_id = new_owner_id
             self.session.commit()
             return selected_pet
         return None
+
+    def refresh_session(self):
+        self.session.close()
+        self.session = Session(self.engine)
 
 
 
--- repository/user_repository.py
+++ repository/user_repository.py
@@ -9,21 +9,25 @@ class UserRepository:
         self.session = Session(engine)
 
     def get_user_by_id(self, user_id):
+        self.refresh_session()
         user = self.session.query(UserModel).filter(UserModel.id == user_id).first()
         if not user:
             return None
         return user
 
     def get_all_users(self):
+        self.refresh_session()
         result = self.session.query(UserModel).all()
         return result
 
     def create_user(self, user_data: UserModel):
+        self.refresh_session()
         self.session.add(user_data)
         self.session.commit()
         return user_data
 
     def delete_user(self, user_id):
+        self.refresh_session()
         selected_user = self.get_user_by_id(user_id)
         if selected_user:
             self.session.delete(selected_user)
@@ -31,7 +35,9 @@ class UserRepository:
             return f"User {user_id} deleted."
         return "No such ID"
 
-
+    def refresh_session(self):
+        self.session.close()
+        self.session = Session(self.engine)
 
 
 
